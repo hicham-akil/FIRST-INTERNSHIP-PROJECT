eef2cacf76b2723fa632b5748f822004376bdb2d
hichamaki <hichamakil2018@gmail.com>
2025-03-26 15:10:10 +0100

    add Statistic migration and model

---
+++ backend/app/Models/Statistic.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Laravel\Sanctum\HasApiTokens;
+
+class Statistic extends Model
+{
+    use HasApiTokens;
+    protected $fillable = [
+        'total_projects',
+        'accepted_projects',
+        'rejected_projects',
+        'pending_projects',
+        'generated_at',
+    ];
+}
