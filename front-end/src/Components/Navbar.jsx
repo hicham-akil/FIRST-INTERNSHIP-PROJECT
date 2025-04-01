8dde85707bc0de1e17ebc5535dae38853bd78811
hichamaki <hichamakil2018@gmail.com>
2025-04-01 18:23:45 +0000

    fixing navbar show avaibiltiy

--- front-end/src/Components/Navbar.jsx
+++ front-end/src/Components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 const Navbar = () => {
+  const isadmine=JSON.parse(localStorage.getItem('isadmine'));
   return (
     <div className='bg-gray-300  flex justify-between   items-center px-4  ' style={{height:"50px"}}>
          <div className='ml-30' >
@@ -9,9 +10,15 @@ const Navbar = () => {
         <div className='mr-30'>
             <Link to={"/Signin"} className='ml-30 hover:font-bold text-gray-500'>Signin</Link>
             <Link to={"/Signup"} className='ml-10 hover:font-bold text-gray-500'>Signup</Link>
-            <Link to={"/CreateProject"} className='ml-10 hover:font-bold text-gray-500'>create Project</Link>
-            <Link to={"/notification"} className='ml-10 hover:font-bold text-gray-500'>Notification</Link>
-            <Link to={"/statistics"} className='ml-10 hover:font-bold text-gray-500'>statistics</Link>
+           {isadmine ?(
+             <Link to={"/statistics"} className='ml-10 hover:font-bold text-gray-500'>statistics</Link>
+            ):(
+              <>
+              <Link to={"/CreateProject"} className='ml-10 hover:font-bold text-gray-500'>create Project</Link>
+              <Link to={"/notification"} className='ml-10 hover:font-bold text-gray-500'>Notification</Link>
+              </>
+            )}
+
         </div>
     </div>
   )
