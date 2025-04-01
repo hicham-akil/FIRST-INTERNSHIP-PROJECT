0280ec90f6712752b07b4e20c1c33a57d2d5f52f
hichamaki <hichamakil2018@gmail.com>
2025-04-01 19:05:54 +0000

    add home page

---
+++ front-end/src/Components/Home.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <h1>hellow here you can take reservation to do any project you want</h1>
+        <img src="" alt="" />
+        <div>
+            <button>go add project</button>
+            
+        </div>
+        <div>
+            <button>go see project</button>
+            
+        </div>
+        <div>
+            <button>go to statistics</button>
+            
+        </div>
+        <div>
+            <button>go contact owner</button>  
+        </div>
+
+    </div>
+  )
+}
+
+export default Home
